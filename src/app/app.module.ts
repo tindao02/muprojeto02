bd0c9c23e004631c64836a922bd7d68c15e0d6a7
Manoel Tindao <timdao02@hotmail.com>
2020-10-06 20:52:38 -0300

    Criação do peojeto e primeiro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { HelloComponent } from './hello/hello.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-hello',
+    template: `
+        <h2>
+            teste
+        </h2>
+    `
+})
+
+export class HelloComponent
+{
+    private nome: string = 'Thiago';
+
+    public getNome(): string
+    {
+        return this.nome;
+    }
+    public setNome(nome: string): void
+    {
+        this.nome = nome;
+    }
+}
